35f329676804fb5d2bed3c1849163a293ac80a69
ampm <>
2023-07-08 16:06:05 +0100

    Home controller and welcome view: load and converted welcome view on Home controller

--- proj/app/Controllers/Home.php
+++ proj/app/Controllers/Home.php
@@ -6,6 +6,12 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message', ['title' => 'Welcome to CI 4']);
+        $title = 'Welcome to CI 4 + Smarty';
+
+        // return view('welcome_message', ['title' => 'Welcome to CI 4']);
+        return $this->smartyDisplay(
+            view: 'welcome_message',
+            params: compact('title')
+        );
     }
 }
